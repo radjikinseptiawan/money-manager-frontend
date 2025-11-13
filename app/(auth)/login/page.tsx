2597d86f2bf6bf524afabe1ad90d500d8d52b138
Radjikin Septiawan <radjikins21@gmail.com>
2025-11-13 12:34:05 +0700

    fix more

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -17,7 +17,7 @@ export default function Page(){
     const loginToAccount = async(e : FormEvent)=>{
         e.preventDefault()
         try{
-            const response = await fetch(`http://localhost:3000/users/login`,{
+            const response = await fetch(`${url}/users/login`,{
                 method:"POST",
                 headers:{"Content-Type" : "application/json"},
                 body:JSON.stringify({
