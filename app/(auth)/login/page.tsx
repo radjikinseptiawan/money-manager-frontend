dfedbd97aed7b8b88764d8962766dade0bcc5461
Radjikin Septiawan <radjikins21@gmail.com>
2025-11-21 09:15:52 +0700

    fix url

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -28,7 +28,7 @@ export default function Page(){
             }
 
             
-            const response = await fetch(`http://localhost:3000/users/login`,{
+            const response = await fetch(`https://api.zeverial.online/users/login`,{
                 method:"POST",
                 headers:{
                     "Content-Type" : "application/json",
--- app/api/dashboard/route.ts
+++ app/api/dashboard/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req : NextRequest){
     const cookie = req.headers.get("cookie")!;
-    const res = await fetch(`http://api.zeverial.online/transaction`, {
+    const res = await fetch(`https://api.zeverial.online/transaction`, {
         method: "GET",
         headers: { 
           "Content-Type": "application/json", 
@@ -17,8 +17,7 @@ export async function GET(req : NextRequest){
 
 export async function POST(req:NextRequest){
     const cookie =req.headers.get("cookie") ?? ""
-    const body = await req.json()
-    const res = await fetch("http://api.zeverial.online/transaction",{
+    const res = await fetch("https://api.zeverial.online/transaction",{
         method:"POST",
            headers: { 
           "Content-Type": "application/json", 
