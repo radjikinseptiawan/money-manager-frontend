c3922fd75ef62354f996a649fac71e93b2760274
Radjikin Septiawan <radjikins21@gmail.com>
2025-12-09 09:21:22 +0700

    first commit

--- app/api/dashboard/route.ts
+++ app/api/dashboard/route.ts
@@ -1,34 +1,41 @@
-import { NextRequest, NextResponse } from "next/server"
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req : NextRequest){
-    const res = await fetch(`https://api.zeverial.online/transaction`, {
-        method: "GET",
-        headers: { 
-          "Content-Type": "application/json", 
-          "x-api-key" : "6B224A9476D91EAF3175184AA4D21"
-        },
-        credentials:"include"
-    })
-        const data =await res.json()
-    return NextResponse.json(data)
+export async function GET(req: NextRequest) {
+  const cookieStore = cookies();
+  const session = (await cookieStore).get("access_token")?.value ?? "";
+
+  const res = await fetch("https://api.zeverial.online/transaction", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": "6B224A9476D91EAF3175184AA4D21",
+      Cookie: `access_token=${session}`
+    },
+    cache: "no-store"
+  });
+
+  const data = await res.json();
+  return NextResponse.json(data);
 }
 
-export async function POST(req:NextRequest){
-    const cookie =req.headers.get("cookie") ?? ""
-    const body = await req.json()
-    const res = await fetch("https://api.zeverial.online/transaction",{
-        method:"POST",
-        headers: { 
-          "Content-Type": "application/json", 
-          "x-api-key" : "6B224A9476D91EAF3175184AA4D21",
-          "Cookie" : cookie,
-        },
-        body:JSON.stringify(body),
-        credentials:"include"
-    })
+export async function POST(req: NextRequest) {
+  const cookieStore = cookies();
+  const session = (await cookieStore).get("access_token")?.value ?? "";
 
-    const data = await res.json()
-    return NextResponse.json(data)
+  const body = await req.json();
+
+  const res = await fetch("https://api.zeverial.online/transaction", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": "6B224A9476D91EAF3175184AA4D21",
+      Cookie: `access_token=${session}`
+    },
+    body: JSON.stringify(body),
+    cache: "no-store"
+  });
+
+  const data = await res.json();
+  return NextResponse.json(data);
 }
-
-
