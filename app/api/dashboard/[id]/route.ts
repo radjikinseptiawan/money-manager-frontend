12f0d877e2b2175ad2050afd97a7d71a68ea5950
Radjikin Septiawan <radjikins21@gmail.com>
2025-12-09 09:25:22 +0700

    fix delete

--- app/api/dashboard/[id]/route.ts
+++ app/api/dashboard/[id]/route.ts
@@ -1,21 +1,23 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req : NextRequest,context : {params : Promise<{ id : string}>}){
-    const cookieStore = cookies();
-    const session = (await cookieStore).get("access_token")?.value ?? "";
-    const {id} = await context.params;
+export async function DELETE(
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  const cookieStore = cookies();
+  const accessToken = (await cookieStore).get("access_token")?.value ?? "";
+  const { id } = await context.params;
 
-    const res = await fetch(`https://api.zeverial.online/transaction/${id}`,{
-        method:"DELETE",
-        headers: {
-          "x-api-key" : "6B224A9476D91EAF3175184AA4D21",
-          "Cookie" : session
-        },
-        credentials:"include"
-    })
+  const res = await fetch(`https://api.zeverial.online/transaction/${id}`, {
+    method: "DELETE",
+    headers: {
+      "x-api-key": "6B224A9476D91EAF3175184AA4D21",
+      "Cookie": `access_token=${accessToken}`,   // ⭐ FIX PENTING DI SINI
+    },
+    credentials: "include",
+  });
 
-    
   const data = await res.json();
   return NextResponse.json(data);
 }
