d83fa669ebb7285d11779086e32ea81ca7c09c6d
Radjikin Septiawan <radjikins21@gmail.com>
2025-12-09 08:56:22 +0700

    add

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return[
+      {
+        source:"/api/dashboard/:api*",
+        destination:"https://api.zeverial.online/transaction/path"
+      }
+    ]
+  },
 };
 
 export default nextConfig;
